b21fdda859276a0ce94a76bba777094536a42152
sakompella <87831937+sakompella@users.noreply.github.com>
2025-10-11 16:52:50 -0400

    storage provider

--- backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
+++ backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
@@ -1,5 +1,6 @@
 package com.bostonhacks.backend;
 
+import com.google.genai.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,12 +18,19 @@ public class RequestController {
   }
 
   @PostMapping("/upload-image")
-  public String uploadImage(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+  public String uploadImage(
+      @RequestParam("file") MultipartFile file,
+      RedirectAttributes redirectAttributes
+  ) {
 
   }
 
   @GetMapping("/image-advice")
-  public String getImageAdvice(@RequestParam("")) {
-
+  public String getImageAdvice(@RequestParam("") String filename) {
+    client.models.generateContent(
+        "gemini-2.5-flash",
+        "Please search this image and examine if there's any personally identifiable information.",
+        null
+    )
   }
 }
